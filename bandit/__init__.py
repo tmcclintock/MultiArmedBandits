c37cd3559ea7adad8962029f9e1155de3d259438
Tom McClintock <tmcclintock89@gmail.com>
2020-05-23 10:27:25 -0400

    Implemented and tested a RandomBandit

--- bandit/__init__.py
+++ bandit/__init__.py
@@ -1,3 +1,3 @@
-from bandit.agent import GreedyAgent  # noqa: F401
+from bandit.agent import RandomBandit  # noqa: F401
 from bandit.reward import GaussianReward, PoissonReward  # noqa: F401
 from bandit.environment import Environment  # noqa: F401
