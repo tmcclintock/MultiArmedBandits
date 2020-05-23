7a641774b090cd2e73b3223dfd58b7cd126018be
Tom McClintock <tmcclintock89@gmail.com>
2020-05-23 08:13:00 -0400

    black formatting.

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Setup file for my multi-armed bandit work.
+"""
+from distutils.core import setup
+
+setup(
+    name="bandit",
+    version="0.1",
+    description="Multi-armed bandit implementations.",
+    author="Thomas McClintock",
+    author_email="thmsmcclintock@gmail.com",
+    url="https://github.com/tmcclintock/MultiArmedBandits",
+    packages=["bandit"],
+)
