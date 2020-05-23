5f72799b9048f3748f8e97d716176df067012ec0
Tom McClintock <tmcclintock89@gmail.com>
2020-05-23 10:35:54 -0400

    Implemented the RandomBandit and tests.

---
+++ tests/test_bandits.py
@@ -0,0 +1,34 @@
+"""
+Tests of bandits.
+"""
+
+from unittest import TestCase
+
+from bandit.agent import RandomBandit
+from bandit.environment import Environment
+from bandit.reward import GaussianReward
+
+
+class TestRandomBandit(TestCase):
+    def setUp(self):
+        super().setUp()
+        N = 5
+        self.n_rewards = N
+        self.env = Environment([GaussianReward() for _ in range(N)])
+
+    def test_smoke(self):
+        b = RandomBandit(self.env)
+        assert isinstance(b, RandomBandit)
+        assert isinstance(b.environment, Environment)
+        assert len(b.environment) == self.n_rewards
+
+    def test_choose_action(self):
+        b = RandomBandit(self.env)
+        assert isinstance(b.choose_action(), int)
+
+    def test_action(self):
+        b = RandomBandit(self.env)
+        a = b.action()
+        assert isinstance(b.action(), float)
+        a2 = b.action()
+        assert a != a2  # unless we get very unlucky
