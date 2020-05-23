7f567bea28c74885a59f625b9f08852bb0050ece
Tom McClintock <tmcclintock89@gmail.com>
2020-05-23 08:49:32 -0400

    Made the reward objects.

---
+++ bandit/reward.py
@@ -0,0 +1,62 @@
+"""
+Classes for the environment and the reward model.
+"""
+
+from typing import Callable
+
+import numpy as np
+import scipy.stats as ss
+
+from abc import ABC, abstractmethod
+
+
+class BaseReward(ABC):
+    """
+    Base class for rewards
+
+    Args:
+        dist (Callable): a random variable distribution that has an `rvs`
+            method that returns a reward
+    """
+
+    def __init__(self, dist: Callable):
+        assert hasattr(dist, "rvs"), "distribution must have rvs() method"
+        self.dist = dist
+
+    @abstractmethod
+    def get_reward(self):
+        return self.dist.rvs()
+
+
+class GaussianReward(BaseReward):
+    """
+    A Gaussian random variable as a reward.
+
+    Args:
+        mean (float): mean of the Gaussian reward
+        var (float): variance of the Gaussian reward; must be positive
+    """
+
+    def __init__(self, mean: float = 0, var: float = 1):
+        assert var > 0, "variance must be positive"
+        super().__init__(ss.norm(loc=mean, scale=np.sqrt(var)))
+
+    def get_reward(self):
+        return super().get_reward()
+
+
+class PoissonReward(BaseReward):
+    """
+    Poisson random variable reward.
+
+    Args:
+        mu (float): rate parameter (mean and var)
+        loc (float): constant shift
+    """
+
+    def __init__(self, mu: float = 1, loc: float = 0):
+        assert mu > 0, "poisson rate must be positive"
+        super().__init__(ss.poisson(mu=mu, loc=loc))
+
+    def get_reward(self):
+        return super().get_reward()
