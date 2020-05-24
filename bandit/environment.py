267f763b45dcb5c09c8fe2b20e592405b5ec9409
Tom McClintock <tmcclintock89@gmail.com>
2020-05-24 09:31:35 -0400

    Added docstring comments to the environment.

--- bandit/environment.py
+++ bandit/environment.py
@@ -23,12 +23,36 @@ class Environment:
         return len(self.rewards)
 
     def action(self, i: int) -> Union[float, int]:
+        """
+        Given a choice of action, produce a reward for that aciton.
+
+        Args:
+            i (int): action to be taken
+
+        Returns:
+            (float) reward from that action
+        """
         assert i > -1
         assert i < self.__len__()
         return self.rewards[i].get_reward()
 
     def expected_rewards(self) -> float:
+        """
+        Produce the expected rewards for all possible actions.
+
+        Returns:
+            (List[float]) expected rewards (true values)
+        """
         return np.array([r.expected_reward() for r in self.rewards])
 
     def moments(self, kind: str = "mv") -> List[float]:
+        """
+        Statistical moments of all actions.
+
+        Args:
+            kind (str): which moments to compute; default is "mv"
+
+        Returns:
+            (np.ndarray) statistical moments of actions
+        """
         return np.array([r.moments(kind) for r in self.rewards])
