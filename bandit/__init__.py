5c6b52373a261ab7aeef99b080458feb4057635d
Tom McClintock <tmcclintock89@gmail.com>
2020-05-25 20:17:37 -0400

    Added figure routines to the init.

--- bandit/__init__.py
+++ bandit/__init__.py
@@ -4,4 +4,8 @@ from bandit.bandit import (  # noqa: F401
     RandomBandit,
 )
 from bandit.environment import Environment  # noqa: F401
+from bandit.figures import (  # noqa: F401
+    plot_average_rewards,
+    plot_reward_distributions,
+)
 from bandit.reward import GaussianReward, PoissonReward  # noqa: F401
