a05c4864fd0589602aae7a5cdc468faa2e89a165
Tom McClintock <tmcclintock89@gmail.com>
2020-06-04 18:18:50 -0400

    Moved conf.py back into source. Added another path to try to find the repo.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
@@ -48,10 +49,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
